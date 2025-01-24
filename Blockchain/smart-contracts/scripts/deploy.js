853a0e94a957e38ca5c1ff242d1b596bfdae5709
Swigstan1810 <swigstanwe@gmail.com>
2025-01-24 15:41:18 +0530

    files

---
+++ Blockchain/smart-contracts/scripts/deploy.js
@@ -0,0 +1,16 @@
+async function main() {
+    const [deployer] = await ethers.getSigners();
+    console.log("Deploying contracts with the account:", deployer.address);
+
+    const Token = await ethers.getContractFactory("HeightsToken");
+    const token = await Token.deploy(1000000); // 1 million tokens
+    console.log("HeightsToken deployed to:", token.address);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
+
