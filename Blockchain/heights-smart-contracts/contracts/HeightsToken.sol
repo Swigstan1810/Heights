48b37ebf2bb314ebe06fcd9883e5aefcaddf09a8
Swigstan1810 <swigstanwe@gmail.com>
2025-03-29 21:02:08 +1000

    Block connected Frontend Test Completegit status

--- Blockchain/heights-smart-contracts/contracts/HeightsToken.sol
+++ Blockchain/heights-smart-contracts/contracts/HeightsToken.sol
@@ -4,24 +4,62 @@ pragma solidity ^0.8.18;
 contract HeightsToken {
     string public name = "Heights Token";
     string public symbol = "HTK";
+    uint8 public decimals = 18;
     uint256 public totalSupply;
     address public owner;
 
     mapping(address => uint256) public balances;
+    mapping(address => mapping(address => uint256)) public allowances;
+
+    // Events
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
 
     constructor(uint256 _initialSupply) {
-        totalSupply = _initialSupply;
+        totalSupply = _initialSupply * 10**decimals; // Convert to smallest units
         owner = msg.sender;
-        balances[msg.sender] = _initialSupply;
+        balances[msg.sender] = totalSupply;
+        emit Transfer(address(0), msg.sender, totalSupply);
     }
 
-    function transfer(address _to, uint256 _amount) public {
+    function transfer(address _to, uint256 _amount) public returns (bool) {
+        require(_to != address(0), "Transfer to zero address");
         require(balances[msg.sender] >= _amount, "Not enough tokens");
+        
         balances[msg.sender] -= _amount;
         balances[_to] += _amount;
+        
+        emit Transfer(msg.sender, _to, _amount);
+        return true;
+    }
+
+    function approve(address _spender, uint256 _amount) public returns (bool) {
+        require(_spender != address(0), "Approve to zero address");
+        
+        allowances[msg.sender][_spender] = _amount;
+        emit Approval(msg.sender, _spender, _amount);
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _amount) public returns (bool) {
+        require(_from != address(0), "Transfer from zero address");
+        require(_to != address(0), "Transfer to zero address");
+        require(balances[_from] >= _amount, "Not enough tokens");
+        require(allowances[_from][msg.sender] >= _amount, "Not enough allowance");
+        
+        balances[_from] -= _amount;
+        balances[_to] += _amount;
+        allowances[_from][msg.sender] -= _amount;
+        
+        emit Transfer(_from, _to, _amount);
+        return true;
     }
 
     function balanceOf(address _account) public view returns (uint256) {
         return balances[_account];
+    }
+
+    function allowance(address _owner, address _spender) public view returns (uint256) {
+        return allowances[_owner][_spender];
     }
 }
--- Blockchain/heights-smart-contracts/hardhat.config.js
+++ Blockchain/heights-smart-contracts/hardhat.config.js
@@ -1,6 +1,33 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+// Make sure to create a .env file with these variables
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0x0000000000000000000000000000000000000000000000000000000000000000";
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "https://ethereum-sepolia.publicnode.com";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      chainId: 31337,
+    },
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 11155111,
+    },
+    // Add more networks as needed
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
 };
